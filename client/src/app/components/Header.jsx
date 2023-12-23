c171a0967091e56383a34833537ce17b1fd32fc6
Baron <bkkcreative23@gmail.com>
2023-12-23 08:40:10 -0600

    image slider done

--- client/src/app/components/Header.jsx
+++ client/src/app/components/Header.jsx
@@ -3,41 +3,39 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <Link href="/">
-      <header class="bg-slate-200 shadow-md">
-        <nav class="flex justify-between items-center max-w-6xl mx-auto p-3">
-          <h1 class="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span class="text-slate-500">Sahand</span>
-            <span class="text-slate-700">Estate</span>
-          </h1>
-          <form class="flex bg-slate-100 items-center p-3 rounded-lg">
-            <input
-              type="text"
-              placeholder="Search..."
-              class="text-xs sm:text-sm bg-transparent focus:outline-none w-24 sm:w-64"
+    <header class="bg-slate-200 shadow-md">
+      <nav class="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 class="font-bold text-sm sm:text-xl flex flex-wrap">
+          <span class="text-slate-500">Sahand</span>
+          <span class="text-slate-700">Estate</span>
+        </h1>
+        <form class="flex bg-slate-100 items-center p-3 rounded-lg">
+          <input
+            type="text"
+            placeholder="Search..."
+            class="text-xs sm:text-sm bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+        </form>
+        <ul class="flex gap-4">
+          <Link href="/">
+            <li class="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link href="/about">
+            <li class="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link href="/about">
+            <img
+              src="https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg"
+              alt="user"
+              class="h-7 w-7 rounded-full object-cover"
             />
-          </form>
-          <ul class="flex gap-4">
-            <Link href="/">
-              <li class="hidden sm:inline text-slate-700 hover:underline">
-                Home
-              </li>
-            </Link>
-            <Link href="/about">
-              <li class="hidden sm:inline text-slate-700 hover:underline">
-                About
-              </li>
-            </Link>
-            <Link href="/about">
-              <img
-                src="https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg"
-                alt="user"
-                class="h-7 w-7 rounded-full object-cover"
-              />
-            </Link>
-          </ul>
-        </nav>
-      </header>
-    </Link>
+          </Link>
+        </ul>
+      </nav>
+    </header>
   );
 };
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Hero } from "./components/Hero";
+import { ImageSlider } from "./components/ImageSlider";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <ImageSlider />
     </main>
   );
 }
